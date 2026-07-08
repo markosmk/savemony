2c1e67b42228cd4ccb104b08982d472617804ccc
Markos Mk <markosmk@me.com>
2026-07-08 12:46:23 -0300

    fix: init index not override class

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,13 +1,13 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
-import { devtools } from "@tanstack/devtools-vite";
+// import { devtools } from "@tanstack/devtools-vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
-    plugins: [devtools(), tailwindcss(), tanstackRouter({ target: "react", autoCodeSplitting: true }), react()],
+    plugins: [tailwindcss(), tanstackRouter({ target: "react", autoCodeSplitting: true }), react()],
     resolve: {
       tsconfigPaths: true,
       alias: {
